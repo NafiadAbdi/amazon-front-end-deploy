e3a0dc3d0a10d15dbc16e0dad0efe602db7049ef
Nafiad <111278128+NafiadAbdi@users.noreply.github.com>
2024-05-29 11:04:19 -0500

    footer added

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
 import LayOut from "../../Components/LayOut/LayOut";
+import BackFooter from "../../Components/Footer/BackFooter"
+import ClosingFooter from "../../Components/Footer/ClosingFooter";
+import MainFooter from "../../Components/Footer/MainFooter";
+import {footerSections} from "../../Components/Footer/FooterData";
+import LastFooter from "../../Components/Footer/LastFooter";
+import {footerItems} from "../../Components/Footer/Fdata";
+
 import Carousel from "../../Components/Carousel/Carousel";
 import Category from "../../Components/Category/Category";
 import Product from "../../Components/Product/Product";
@@ -10,6 +17,10 @@ function Landing() {
 			<Carousel />
 			<Category />
 			<Product />
+			<BackFooter />
+			<MainFooter sections={footerSections} />
+			<ClosingFooter />
+			<LastFooter items={footerItems} />
 		</LayOut>
 	);
 }
